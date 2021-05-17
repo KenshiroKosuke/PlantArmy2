5fd2ed83350fc019aa7d5c6c89bd2d08524e9835
mahasamut ngeontaweekoon <mahasamud555@gmail.com>
2021-05-17 20:08:38 +0700

    new button style

--- src/logic/BuyPlantButton.java
+++ src/logic/BuyPlantButton.java
@@ -5,6 +5,8 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Border;
@@ -18,26 +20,32 @@ import entity.Sunflower;
 import entity.base.Plant;
 public class BuyPlantButton extends Button {
 	private Plant plant;
+	private String buttonUrl;
 	public BuyPlantButton(String plantName) {
 		super();
 		switch(plantName) {
-		case "PeaShooter": this.plant = new PeaShooter(); 	break;
-		case "Sunflower" : this.plant = new Sunflower(); 	break;
+		case "PeaShooter": this.plant = new PeaShooter(); this.buttonUrl = "Button_PeaShooter.png";	break;
+		case "Sunflower" : this.plant = new Sunflower(); this.buttonUrl = "Button_Sunflower.png";	break;
 		default			 : this.plant = null;				break;
 		}
-		this.setPrefHeight(100);
-		this.setPrefWidth(200);
+		//this.setPrefHeight(100);
+		//this.setPrefWidth(200);
+		String image_path = ClassLoader.getSystemResource(buttonUrl).toString();
+	    ImageView view = new ImageView(new Image(image_path));
+	    this.setGraphic(view);
+	    this.setStyle("-fx-background-color: transparent;");
 		//use plant name to set img
-		this.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
-		this.setBorder(new Border(
-				new BorderStroke(Color.GRAY, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+	    // ---> They are different images so let's put each button images here
+		//this.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+		//this.setBorder(new Border( new BorderStroke(Color.GRAY, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
 	}
 	public void highlight() {
-		this.setBackground(new Background(new BackgroundFill(Color.LIMEGREEN, CornerRadii.EMPTY, Insets.EMPTY)));
+		this.setOpacity(0.5);
 	}
 
 	public void unhighlight() {
-		this.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+		//this.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+		this.setOpacity(1.0);
 	}
 	public Plant getPlant() {
 		return plant;
